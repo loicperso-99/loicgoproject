ab20ad51070503fd6c5e669597c6e8a8d02e6fc9
Loic Raucy <loic.raucy@datadoghq.com>
2024-09-19 13:44:35 +0200

    Add comment.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// main
 	fmt.Println("vim-go")
 	fmt.Println("a", "a", "a", "warniiiing                ", "warniiiing                ", "warniiiing                ", "warniiiing                ", "warniiiing                ", "warniiiing                ", "warniiiing                ")
 	fmt.Println("a", "a", "a")
